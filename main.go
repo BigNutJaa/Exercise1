e049a968f661103ddd718a8eddc40884ddcec0e2
BigNutJaa <akkhaphat.i@hotmail.com>
2022-10-03 13:37:46 +0700

    newCode

--- main.go
+++ main.go
@@ -1,39 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
+	"github.com/BigNutJaa/generateName"
+	"log"
 )
 
 func main() {
-	var name string
-	var number string
 
-	for {
-		fmt.Println("Enter your name:")
-		fmt.Scanln(&name)
-		if _, err := strconv.ParseFloat(name, 64); err == nil {
-			fmt.Printf("%q looks like a number.\n", name)
-		} else {
-			break
-
-		}
+	result, err := generateName.Generate("bignut", "12.2")
+	if err != nil {
+		log.Println("error:", err)
 	}
-
-	for {
-		fmt.Println("Enter your number:")
-		fmt.Scanln(&number)
-		if _, err := strconv.ParseFloat(number, 64); err != nil {
-			fmt.Printf("%q not a number.\n", number)
-		} else {
-			break
-
-		}
-	}
-
-	var nameUPPER string = strings.ToUpper(name)
-	new, _ := strconv.ParseFloat(number, 64)
-	fmt.Printf("%v%v%03d", nameUPPER, "-", int(new))
-
+	log.Println(result)
 }
